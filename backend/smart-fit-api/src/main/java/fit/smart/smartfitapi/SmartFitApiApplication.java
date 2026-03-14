85c1cc5bb001838338a7ef87f0a970cd08d949c8
YOUSFI Btissam <ibtissamyousfi111@gmail.com>
2026-03-14 16:14:23 +0000

    init project

---
+++ backend/smart-fit-api/src/main/java/fit/smart/smartfitapi/SmartFitApiApplication.java
@@ -0,0 +1,13 @@
+package fit.smart.smartfitapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFitApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartFitApiApplication.class, args);
+    }
+
+}
---
+++ backend/smart-fit-api/src/test/java/fit/smart/smartfitapi/SmartFitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fit.smart.smartfitapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFitApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
