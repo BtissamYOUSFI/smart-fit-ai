39bf152591ee86162fe27f211ff0fc698cf03aba
YOUSFI Btissam <ibtissamyousfi111@gmail.com>
2026-05-25 19:18:42 +0100

    save local changes

--- frontend/constants/config.ts
+++ frontend/constants/config.ts
@@ -1,9 +1,9 @@
 // export const API_URL = "http://localhost:8080/api";
-
+//
 // import { Platform } from "react-native";
-
+//
 // const LOCAL_IP = "localhost";
-
+//
 // export const API_URL =
 //     Platform.OS === "android"
 //         ? "http://10.0.2.2:8080/api"
