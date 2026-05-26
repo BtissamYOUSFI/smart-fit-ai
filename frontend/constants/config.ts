53667cee59b342d45dd33af6dd36c9bc5ed242b4
YOUSFI Btissam <ibtissamyousfi111@gmail.com>
2026-05-26 11:47:35 +0100

    chore: remove env file from tracking

--- frontend/constants/config.ts
+++ frontend/constants/config.ts
@@ -1,15 +1,15 @@
 // export const API_URL = "http://localhost:8080/api";
-
-// import { Platform } from "react-native";
+// //
+// // import { Platform } from "react-native";
+// //
+// // const LOCAL_IP = "localhost";
+// //
+// // export const API_URL =
+// //     Platform.OS === "android"
+// //         ? "http://10.0.2.2:8080/api"
+// //         : `http://${LOCAL_IP}:8080/api`;
 //
-// const LOCAL_IP = "localhost";
 //
-// export const API_URL =
-//     Platform.OS === "android"
-//         ? "http://10.0.2.2:8080/api"
-//         : `http://${LOCAL_IP}:8080/api`;
-
-
 
 
 const PUBLIC_IP = "54.221.13.194";
